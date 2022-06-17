54565baf68f13e8f3030a959e59178d4735cef59
tgparkk <sttgpark@gmail.com>
2022-06-17 13:22:41 +0900

    test

---
+++ boost_test/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <boost/beast.hpp>
+
+int32_t main()
+{
+    std::cout << "BOOST version : " << BOOST_VERSION << "\n";
+    //std::cout << "BOOST version : " << 1<< "\n";
+
+    return 0;
+}
