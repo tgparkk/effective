4bad41f4f98b776e81da3df288c4fb0a59396bd2
tgparkk <sttgpark@gmail.com>
2022-06-17 14:17:43 +0900

    cmake_boost
    http://daplus.net/boost-cmake%EA%B0%80-boost%EB%A5%BC-%EC%B0%BE%EC%A7%80-%EB%AA%BB%ED%95%A8/
    
    https://hbase.tistory.com/221

---
+++ cmake_boost/cmake_boost/cmake_boost.cpp
@@ -0,0 +1,11 @@
+﻿// cmake_boost.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "cmake_boost.h"
+#include <boost/beast.hpp>
+
+int main()
+{
+	std::cout << "BOOST version : " << BOOST_VERSION << "\n";
+	return 0;
+}
---
+++ cmake_boost/cmake_boost/cmake_boost.h
@@ -0,0 +1,8 @@
+﻿// cmake_boost.h: 표준 시스템 포함 파일
+// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: 여기서 프로그램에 필요한 추가 헤더를 참조합니다.
