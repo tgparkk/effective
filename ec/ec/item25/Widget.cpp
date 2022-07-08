67b9bf548d93d0e0746ff9d6d3b4634e766eaee9
tgparkk <sttgpark@gmail.com>
2022-07-08 15:50:12 +0900

    total template specialization

---
+++ ec/ec/item25/Widget.cpp
@@ -0,0 +1,13 @@
+#include "Widget.h"
+
+WidgetImpl::WidgetImpl()
+	:a(1), b(2), c(3), v()
+{
+
+}
+
+Widget::Widget(const Widget& rhs)
+	:pImpl()
+{
+
+}
---
+++ ec/ec/item25/Widget.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include "../pch.h"
+
+class WidgetImpl {			// class for Widget data;
+public:						// details are unimportant
+	WidgetImpl();
+	
+private:
+	int a, b, c;			// possibly lots of data —
+	std::vector<double> v;	// expensive to copy!
+	
+};
+
+class Widget { // class using the pimpl idiom
+public:
+	Widget(const Widget& rhs);
+	Widget& operator=(const Widget& rhs)	// to copy a Widget, copy its
+	{										// WidgetImpl object. For ... // details on implementing
+		*pImpl = *(rhs.pImpl);				// operator= in general, ... 
+											// see Items 10, 11, and 12. 
+	}
+
+	void swap(Widget& other)
+	{
+		using std::swap;
+
+		swap(pImpl, other.pImpl);
+	}
+
+private:
+	WidgetImpl* pImpl; // ptr to object with this 
+};
+
+/**/
+namespace std {
+	template<>
+	void swap<Widget>(Widget& a, Widget& b)
+	{
+		a.swap(b);
+	}
+}
